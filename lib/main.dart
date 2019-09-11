301b1860ce0d67b82dfd8228d253990fde81b710
shahrukhktk <shahrukhktk760@gmail.com>
2019-09-11 22:33:53 +0500

    final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-            title: Text('I Am Rich'),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          body: Center(
-            child: Image(
-              image: NetworkImage(
-                  'http://pngimg.com/uploads/diamond/diamond_PNG6701.png'),
+            backgroundColor: Colors.blueGrey,
+            appBar: AppBar(
+              title: Text('I Am Rich'),
+              backgroundColor: Colors.blueGrey[900],
             ),
-          ),
-        ),
+            body: Image(image: AssetImage('images/tasks.png'))),
       ),
     );
